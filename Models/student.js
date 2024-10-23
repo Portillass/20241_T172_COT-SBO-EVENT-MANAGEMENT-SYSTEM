594fd96ae1e1351802745a590fc74b02bca9c601
jarell <portillasjep@gmail.com>
2024-10-23 22:59:35 +0800

    COT-SBO EVENT MANAGEMENT SYSTEM

---
+++ Models/student.js
@@ -0,0 +1,11 @@
+// thisis the mobile phone that only they can scan qr code 
+//======================== STUDENT=======================//
+
+// can Scan Qr code 
+app.get();
+
+// Get all events
+app.get();
+
+// Get event by time and date
+app.get();
