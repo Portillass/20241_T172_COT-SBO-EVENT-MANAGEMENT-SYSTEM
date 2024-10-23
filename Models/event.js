6bdb24bca51cb3897410ee8291c89689ea45863f
jarell <portillasjep@gmail.com>
2024-10-23 23:17:06 +0800

    COT-SBO EVENT MANAGEMENT SYSTEM

--- Models/event.js
+++ Models/event.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const eventSchema = new mongoose.Schema({
   name: String,
   date: Date,
-  // ... other event properties
+
 });
 
 const Event = mongoose.model('Event', eventSchema);
